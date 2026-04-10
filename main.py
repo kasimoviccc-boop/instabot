4676db5d852ab33e75fca5295d71f7fd04251858
kasimoviccc-boop <kasimoviccc@gmail.com>
2026-04-10 20:51:58 +0500

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from instagrapi import Client
 API_TOKEN = '8618465943:AAEyXXvV8nBsfsgS09XE7ehT8cpydO9WutU'
 ADMIN_ID = '6052580480'
 INSTA_USER = 'BOTIMIZ1'
-INSTA_PASS = 'KASIMOV1'
+INSTA_PASS = 'KASIMOV2'
 SESSION_FILE = "insta_session.json"
 
 logging.basicConfig(level=logging.INFO)
